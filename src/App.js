f6bf0ba1dfafc12a91abab2b53cdbeab54d32590
brytebee <brytebee@gmail.com>
2022-02-09 09:21:51 +0100

    :white_check_mark: Update App.js
    Refactor from class based component to function based conponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { Component } from 'react';
 import Calculator from './components/calculator';
 
-/* eslint-disable react/prefer-stateless-function */
-class App extends Component {
-  /* eslint-enable react/prefer-stateless-function */
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
