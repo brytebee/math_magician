6b5c6bf83cf200f4f261576ec47f8b6b02188d5a
brytebee <brytebee@gmail.com>
2022-02-16 23:01:51 +0100

    :stethoscope: Add snapshots
    Added snapshots of all components and iinteractions with them

---
+++ src/__tests__/component.test.js
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom';
+import { cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Index from '../pages/Index';
+import Calc from '../pages/Calc';
+import Quotes from '../pages/Quotes';
+import Error from '../pages/Error';
+import Footer from '../components/Footer';
+
+afterEach(cleanup);
+
+describe('Snapshot of Calculator component', () => {
+  it('Renders the Index page', () => {
+    const tree = renderer.create(<Index />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders the Calc page', () => {
+    const tree = renderer.create(<Calc />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders the Error page', () => {
+    const tree = renderer.create(<Error />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders the Quotes page', () => {
+    const tree = renderer.create(<Quotes />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders the Footer component', () => {
+    const tree = renderer.create(<Footer />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
